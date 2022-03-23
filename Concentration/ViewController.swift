2f7192cb4cc0981e0aad40ccd9c47d244a8b7301
Rhett L <webster117@hotmail.com>
2022-03-23 12:51:16 -0600

    added ghost and pumpkin buttons

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
     }
-
-
+    
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🎃", on: sender)
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = UIColor.orange
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = UIColor.white
+        }
+    }
+    
 }
 
